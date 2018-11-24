ef869c48969fd337c2216f2dd6ea8b0e2c0343e3
Anna <awasome.sabrielle@ya.ru>
2018-11-24 15:02:43 +0300

    Компоненты для опросов (контролы, контейнер, элементы голосования)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import Main from './Components/Main'
 import './App.css';
+import { cn } from '@bem-react/classname';
+
+const cnApp = cn("App");
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className={cnApp()}>
         <Main></Main>
       </div>
     );
