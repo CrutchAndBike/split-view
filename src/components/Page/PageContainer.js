fba47583bbde037df2039f53a05925cecfbfa6bf
Vladislav Telichko <vladislav.telichko@ya.ru>
2018-12-05 21:56:57 +0300

    Delete all components

--- src/components/Page/PageContainer.js
+++
@@ -1,8 +0,0 @@
-import { connect } from 'react-redux';
-import PageComponent from './PageComponent';
-
-const mapStateToProps = () => ({ splitViewStore: { userInfo } }) => ({
-	userInfo
-});
-
-export default connect(mapStateToProps)(PageComponent);
--- src/components/SplitView/SplitViewContainer.js
+++
@@ -1,8 +0,0 @@
-import { connect } from 'react-redux';
-import SplitViewComponent from './SplitViewComponent';
-
-const mapStateToProps = () => ({ splitViewStore: { isFinished } }) => ({
-	isFinished
-});
-
-export default connect(mapStateToProps)(SplitViewComponent);
