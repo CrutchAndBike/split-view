3137f287a64adf9bc563ce417ceae18569a7ccae
Anna <awasome.sabrielle@ya.ru>
2018-12-14 22:42:02 +0300

    Карточки для видео

--- src/components/SplitView/SplitViewComponent.js
+++ src/components/SplitView/SplitViewComponent.js
@@ -10,19 +10,21 @@ class SplitViewComponent extends Component {
     render() {
         const { isFinished, setup, header } = this.props;
         return <div>
-            {!isFinished && <div className={cnPollContainer()}>
+            {!isFinished && <React.Fragment>
+                <div className={cnPollContainer()}>
+                    <h1 className={cnPollContainer('header')}>{header}</h1>
+                    <div className={cnPollContainer('container')}>
+                        {
+                            this.props.answers.map((option, index) =>
+                                <PollContainer key={index} className={cnPollContainer('option')} options={option} />
+                            )
+                        }
+                    </div>
+                </div>
                 <div className={cnPollContainer('controls')}>
                     <PollControls setup={setup}/>
                 </div>
-                <h1 className={cnPollContainer('header')}>{header}</h1>
-                <div className={cnPollContainer('container')}>
-                    {
-                        this.props.answers.map((option, index) =>
-                            <PollContainer key={index} className={cnPollContainer('option')} options={option} />
-                        )
-                    }
-                </div>
-            </div>}
+            </React.Fragment>}
             {isFinished && <span>Finished</span>}
         </div>;
     }
